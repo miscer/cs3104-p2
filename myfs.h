57ad2f752e52b8e8b0db6f4f06cf4e0a3a6689c7
Michal Miškerník <michal@miskernik.sk>
2016-11-13 23:26:13 +0000

    Comments

--- myfs.h
+++ myfs.h
@@ -6,31 +6,35 @@
 #define MY_MAX_OPEN_FILES 1000
 #define MY_MAX_FILE_SIZE MY_MAX_BLOCKS*MY_BLOCK_SIZE
 
+/** @brief File control block */
 struct my_fcb {
-  uuid_t id;    /* file ID */
-  uid_t  uid;   /* user */
-  gid_t  gid;   /* group */
-  mode_t mode;  /* protection */
-  time_t atime; /* time of last access */
-  time_t mtime; /* time of last modification */
-  time_t ctime; /* time of last change to meta-data (status) */
-  nlink_t nlink; /* number of hard links */
-  off_t size;   /* size */
-  uuid_t data;  /* data */
+  uuid_t id; /**< File UUID */
+  uid_t  uid; /**< User ID */
+  gid_t  gid; /**< Group ID */
+  mode_t mode; /**< Protection and file type */
+  time_t atime; /**< Time of last access */
+  time_t mtime; /**< Time of last modification */
+  time_t ctime; /**< Time of last change to meta-data (status) */
+  nlink_t nlink; /**< Number of hard links */
+  off_t size; /**< File data size */
+  uuid_t data; /**< Index block UUID */
 };
 
+/** @brief Index block */
 struct my_index {
-  uuid_t entries[MY_MAX_BLOCKS];
+  uuid_t entries[MY_MAX_BLOCKS]; /**< Array of data block UUIDs */
 };
 
+/** @brief Directory header */
 struct my_dir_header {
-  int items;
-  int first_free;
+  int items; /**< Number of entries in the directory */
+  int first_free; /**< Offset of the first unused entry, if no unused entry */
 };
 
+/** @bried Directory entry */
 struct my_dir_entry {
-  char name[MY_MAX_PATH];
-  uuid_t fcb_id;
-  int next_free;
-  char used;
+  char name[MY_MAX_PATH]; /**< Entry name */
+  uuid_t fcb_id; /**< UUID of the FCB for the entry file **/
+  int next_free; /**< Offset of the next unused entry, if no next entry */
+  char used; /**< Whether this entry is used */
 };
